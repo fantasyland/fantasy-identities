cf4baa60f5a740faaef782ecbca2ee26090805b4
Brian McKenna <brian@precog.com>
2013-05-31 22:09:58 -0500

    Add IdentityT transformer

--- identity.js
+++ identity.js
@@ -19,6 +19,31 @@ Identity.prototype.ap = function(a) {
     });
 };
 
+// Transformer
+Identity.IdentityT = function(M) {
+    var IdentityT = daggy.tagged('run');
+    IdentityT.lift = IdentityT;
+    IdentityT.of = function(a) {
+        return IdentityT(M.of(a));
+    };
+    IdentityT.prototype.chain = function(f) {
+        return IdentityT(this.run.chain(function(x) {
+            return f(x).run;
+        }));
+    };
+    IdentityT.prototype.map = function(f) {
+        return this.chain(function(a) {
+            return IdentityT.of(f(a));
+        });
+    };
+    IdentityT.prototype.ap = function(a) {
+        return this.chain(function(f) {
+            return a.map(f);
+        });
+    };
+    return IdentityT;
+};
+
 // Export
 if(typeof module != 'undefined')
     module.exports = Identity;
